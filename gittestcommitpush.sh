8b8ebede5672082fd14fed9c1b2422b0c573043e
Rjae <rjae.easton@gmail.com>
2020-11-24 09:18:40 -0500

    Fix gittestcommitpush.sh

--- gittestcommitpush.sh
+++ gittestcommitpush.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-dotnet format --check --exclude *.Tests **/Migrations --verbosity quiet || exit 1
-dotnet build || exit 1
-dotnet test -warnaserror --filter Category!=Service || exit 1
+dotnet format --check --exclude *.Tests **/Migrations --verbosity quiet; || exit 1
+dotnet build; || exit 1
+dotnet test -warnaserror --filter Category!=Service; || exit 1
 git add --verbose .
 git commit -m "$1"
 git pull
