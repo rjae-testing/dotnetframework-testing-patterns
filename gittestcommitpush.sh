03ce210432ea028a3ad713604ebb040642a5ae7b
Rjae <rjae.easton@gmail.com>
2020-11-24 09:16:41 -0500

    Add gittestcommitpush.sh

---
+++ gittestcommitpush.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+dotnet format --check --exclude *.Tests **/Migrations --verbosity quiet || exit 1
+dotnet build || exit 1
+dotnet test -warnaserror --filter Category!=Service || exit 1
+git add --verbose .
+git commit -m "$1"
+git pull
+git push --verbose origin master
